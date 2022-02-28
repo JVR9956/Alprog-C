17cf062226b6e3a3f21f7ed333b44a78108bb959
JVR9956 <99193363+JVR9956@users.noreply.github.com>
2022-02-28 18:47:57 +0700

    Update ProcessAName5026211068.java

--- ProcessAName5026211068.java
+++ ProcessAName5026211068.java
@@ -8,17 +8,12 @@ String name = " ";
 
 Scanner sc = new Scanner(System.in);
 
-while(true) {
-
 System.out.print("Type your name: ");
 
 name = sc.nextLine();
 
-if(name.equalsIgnoreCase("quit")) break;
-
 System.out.println("Your name is: "+name.split(" ")[1]+", "+name.split(" ")[0].charAt(0)+".");
 
-}
 
 }
 
